88c081ac7a52ca21b12f12f1379acd338f9cdfb1
Teresa E. Gimeno <tegimeno@gmail.com>
2020-04-30 11:32:51 +0200

    updated script to calculate SWL with data downloaded from google sheets

--- scriptsMA/calcSWL.R
+++ scriptsMA/calcSWL.R
@@ -1,30 +1,32 @@
-swl <- read.csv("dataMA/data_SWLN.csv")
-swl$id <- paste0(swl$author, '_', swl$year, '_', swl$journal)
-swl <- doBy::orderBy(~id, swl)
+library(googlesheets4)
+source <- read_sheet("https://docs.google.com/spreadsheets/d/1udiv5w7YXCZXP2Pjnpshkzxsx5dxV1slG0OX7eztVq4/edit#gid=0")
+source$id <- paste0(source$author, '_', source$year, '_', source$journal)
+source$id_date <- paste0(source$author, '_', source$year, '_', source$journal, '_', source$date)
+source <- doBy::orderBy(~id_date, source)
 
-swl_results <- data.frame(row.names = 1:length(unique(swl$id)))
-swl_results$id <- unique(swl$id)
+source_results <- data.frame(row.names = 1:length(unique(source$id_date)))
+source_results$id_date <- unique(source$id_date)
 
-swlL <- list()
-for(i in 1:nrow(swl_results)){
-  swlL[[i]] <- subset(swl, swl$id == swl_results$id[i])
+sourceL <- list()
+for(i in 1:nrow(source_results)){
+  sourceL[[i]] <- subset(source, source$id_date == source_results$id_date[i])
 }
 
-swlFits <- list()
-for(i in 1:length(swlL)){
-  swlFits[[i]] <- summary(lm(d2H_permil ~ d18O_permil, data = swlL[[i]]))
+sourceFits <- list()
+for(i in 1:length(sourceL)){
+  sourceFits[[i]] <- summary(lm(d2H_permil_source ~ d18O_permil_source, data = sourceL[[i]]))
 }
 
-for (i in 1:nrow(swl_results)){
-  swl_results$intercept <- swlFits[[i]]$coeff[1]
-  swl_results$int_se <- swlFits[[i]]$coeff[3]
-  swl_results$int_t <- swlFits[[i]]$coeff[5]
-  swl_results$int_p <- swlFits[[i]]$coeff[7]
-  swl_results$slope <- swlFits[[i]]$coeff[2]
-  swl_results$slp_se <- swlFits[[i]]$coeff[4]
-  swl_results$slp_t <- swlFits[[i]]$coeff[6]
-  swl_results$slp_p <- swlFits[[i]]$coeff[8]
-  swl_results$r2 <- swlFits[[i]]$r.squared
-  swl_results$N <- nrow(swlL[[i]])
+for (i in 1:nrow(source_results)){
+  source_results$intercept[i] <- sourceFits[[i]]$coeff[1]
+  source_results$int_se[i] <- sourceFits[[i]]$coeff[3]
+  source_results$int_t[i] <- sourceFits[[i]]$coeff[5]
+  source_results$int_p[i] <- sourceFits[[i]]$coeff[7]
+  source_results$slope[i] <- sourceFits[[i]]$coeff[2]
+  source_results$slp_se[i] <- sourceFits[[i]]$coeff[4]
+  source_results$slp_t[i] <- sourceFits[[i]]$coeff[6]
+  source_results$slp_p[i] <- sourceFits[[i]]$coeff[8]
+  source_results$r2[i] <- sourceFits[[i]]$r.squared
+  source_results$N[i] <- nrow(sourceL[[i]])
 }
 
